4378ec30e8b8c288259460bb14a002443331930c
thoriqnoer <thoriq.noer@gmail.com>
2021-05-08 21:06:11 +0700

    Create custom button and styling custom button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,6 +17,9 @@ const Header = () => {
         <Link className='option' to='/about'>
           About
         </Link>
+        <Link className='option' to='/signin'>
+          Sign In
+        </Link>
       </div>
     </div>
   )
---
+++ src/pages/sign-in-and-sign-up/signInAndSignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/signIn.component'
+
+import './signInAndSignUp.styles.scss'
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInAndSignUpPage
